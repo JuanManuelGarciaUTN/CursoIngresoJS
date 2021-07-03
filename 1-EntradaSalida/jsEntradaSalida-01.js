308e0ac7c7b18e358e33d3567bdce891b96033e5
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2021-07-03 00:57:43 -0300

    primer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,11 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla");
+
+
+	//para añadir comentario poner dos barras laterales como estas "//"
+
+	//pude solucionar lo de git <3
 }
 
